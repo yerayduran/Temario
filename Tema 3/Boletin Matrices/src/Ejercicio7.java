ec43796b3f6bf42c74c7b8d6850bd17bb15f5582
yerayduran <yerayduranchaves2006@gmail.com>
2025-10-23 13:23:12 +0200

    yeray
    Signed-off-by: yerayduran <yerayduranchaves2006@gmail.com>

---
+++ Tema 3/Boletin Matrices/src/Ejercicio7.java
@@ -0,0 +1,31 @@
+public class Ejercicio7 {
+    public static boolean sonIguales(int[][] matriz1, int[][] matriz2) {
+        if (matriz1.length != matriz2.length) {
+            return false;
+        }
+        for (int i = 0; i < matriz1.length; i++) {
+            if (matriz1[i].length != matriz2[i].length) {
+                return false;
+            }
+            for (int j = 0; j < matriz1[i].length; j++) {
+                if (matriz1[i][j] != matriz2[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        int[][] matrizA = {{1, 2, 3}, {4, 5, 6}};
+        int[][] matrizB = {{1, 2, 3}, {4, 5, 6}};
+        int[][] matrizC = {{1, 2, 3}, {4, 5, 7}};
+
+        boolean resultado1 = sonIguales(matrizA, matrizB);
+        System.out.println("¿Matriz A es igual a Matriz B?: " + resultado1);
+
+        boolean resultado2 = sonIguales(matrizA, matrizC);
+        System.out.println("¿Matriz A es igual a Matriz C?: " + resultado2);
+    }
+}
---
+++ Tema 3/Boletin Matrices/src/Ejercicio8.java
@@ -0,0 +1,33 @@
+public class Ejercicio8 {
+    public static int sumaResultante(int[][] matriz1, int[][] matriz2) {
+        if (matriz1.length != matriz2.length || matriz1[0].length != matriz2[0].length) {
+            throw new IllegalArgumentException("Las matrices deben tener las mismas dimensiones para ser sumadas.");
+        }
+
+        int[][] matrizResultado = new int[matriz1.length][ matriz1[0].length];
+        int sumaTotal = 0;
+
+        for (int i = 0; i < matriz1.length; i++) {
+            for (int j = 0; j <  matriz1[0].length; j++) {
+                matrizResultado[i][j] = matriz1[i][j] + matriz2[i][j];
+                sumaTotal += matrizResultado[i][j];
+            }
+        }
+
+        System.out.println("Matriz Resultante de la Suma:");
+        for (int i = 0; i < matriz1.length; i++) {
+            for (int j = 0; j <  matriz1[0].length; j++) {
+                System.out.print(matrizResultado[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        return sumaTotal;
+    }
+    public static void main(String[] args) {
+        int[][] matrizA = {{1, 2, 3}, {4, 5, 6}};
+        int[][] matrizB = {{7, 8, 9}, {10, 11, 12}};
+        int suma = sumaResultante(matrizA, matrizB);
+        System.out.println("Suma Total de los Elementos de la Matriz Resultante: " + suma);
+    }
+}
