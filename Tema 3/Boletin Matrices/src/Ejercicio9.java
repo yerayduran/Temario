142871ec0d019e1aba8844578dffe26eab965519
yerayduran <yerayduranchaves2006@gmail.com>
2025-10-23 13:33:27 +0200

    yeray
    Signed-off-by: yerayduran <yerayduranchaves2006@gmail.com>

--- Tema 3/Boletin Matrices/src/Ejercicio9.java
+++ Tema 3/Boletin Matrices/src/Ejercicio9.java
@@ -1,2 +1,21 @@
 public class Ejercicio9 {
+    public static boolean matrizEsSimetricaAlEjeX(int[][] matriz) {
+        for (int i = 0; i < matriz.length / 2; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                if (matriz[i][j] != matriz[matriz.length - 1 - i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+
+    }
+    public static void main(String[] args) {
+        int[][] matriz1 = {{1, 2, 3}, {4, 5, 6}, {1, 2, 3}};
+        int[][] matriz2 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+        System.out.println("¿Matriz 1 es Simetrico?: " + matrizEsSimetricaAlEjeX(matriz1));
+        System.out.println("¿Matriz 2 es Simetrico?: " + matrizEsSimetricaAlEjeX(matriz2));
+    }
 }
+
