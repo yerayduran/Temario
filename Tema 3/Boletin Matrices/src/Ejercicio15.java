b2f3e693d96b046644d38fe9d04edb49c1ab6210
yerayduran <yerayduranchaves2006@gmail.com>
2025-10-29 09:00:04 +0100

    yeray
    Signed-off-by: yerayduran <yerayduranchaves2006@gmail.com>

--- Tema 3/Boletin Matrices/src/Ejercicio15.java
+++ Tema 3/Boletin Matrices/src/Ejercicio15.java
@@ -1,30 +1,34 @@
 import java.util.Scanner;
 
 public class Ejercicio15 {
-    public static boolean pedirNumeroYDevuelvaCoordenadas(int[][] matriz, int numero, int[] coordenadas) {
-        for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[i].length; j++) {
-                if (matriz[i][j] == numero) {
-                    coordenadas[0] = i;
-                    coordenadas[1] = j;
-                    return true;
+    public static void pedirNumeroYDevuelvaCoordenadasSinMatriz(int numero) {
+        int filas = 5;
+        int columnas = 6;
+        boolean encontrado = false;
+
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                int valorActual = i * columnas + j + 1; // Generar valor basado en la posición
+                if (valorActual == numero) {
+                    System.out.println("El número " + numero + " se encuentra en la posición: (" + i + ", " + j + ")");
+                    encontrado = true;
+                    break;
                 }
             }
+            if (encontrado) {
+                break;
+            }
         }
-        return false;
-    }
-    public static void main(String[] args) {
-        int[][] matriz = {{10, 20, 30}, {40, 50, 60}, {70, 80, 90}};
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Ingrese un número para buscar en la matriz: ");
-        int numero = scanner.nextInt();
-        int[] coordenadas = new int[2];
-
-        if (pedirNumeroYDevuelvaCoordenadas(matriz, numero, coordenadas)) {
-            System.out.println("El número " + numero + " se encuentra en las coordenadas: (" + coordenadas[0] + ", " + coordenadas[1] + ")");
-        } else {
+        if (!encontrado) {
             System.out.println("El número " + numero + " no se encuentra en la matriz.");
         }
     }
-}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese un número entre 1 y 30: ");
+        int numero = scanner.nextInt();
+        pedirNumeroYDevuelvaCoordenadasSinMatriz(numero);
+        scanner.close();
+    }
+}   
