01db80b1e8d251201433cc3611c39da0f13c352f
yerayduran <yerayduranchaves2006@gmail.com>
2025-10-29 15:46:45 +0100

    Yeray
    Signed-off-by: yerayduran <yerayduranchaves2006@gmail.com>

--- Tema 3/Boletin Matrices/src/Ejercicio16.java
+++ Tema 3/Boletin Matrices/src/Ejercicio16.java
@@ -1,45 +1,42 @@
 public class Ejercicio16 {
 
-    public static int[][] contarParesPegados(int[][] matriz) {
 
-        int[][] resultado = new int[matriz.length][matriz[0].length];
-
-        int[] dirrx = {-1, -1, -1, 0, 0, 1, 1, 1};
-        int[] dirry = {-1, 0, 1, -1, 1, -1, 0, 1};
-
-        for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[0].length; j++) {
-                int contadorPares = 0;
-
-                for (int k = 0; k < 8; k++) {
-                    int nuevoX = i + dirrx[k];
-                    int nuevoY = j + dirry[k];
-
-                    // Verificar si el vecino está dentro de los límites de la matriz
-                    if (nuevoX >= 0 && nuevoX < matriz.length && nuevoY >= 0 && nuevoY < matriz[0].length) {
-                        if (matriz[nuevoX][nuevoY] % 2 == 0) {
-                            contadorPares++;
-                        }
-                    }
-                }
-
-                resultado[i][j] = contadorPares;
-            }
-        }
-
-        return resultado;
-    }
-    public static void main(String[] args) {
-        int[][] matriz = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-
-        int[][] resultado = contarParesPegados(matriz);
-
-        // Imprimir la matriz resultado
+    public static void main(String[] args) {  //Ejemplo de uso que imprime la matriz resultado
+        int[][] matriz = {{1, 0, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+        int[][] resultado = generarMatrizResultado(matriz);
         for (int i = 0; i < resultado.length; i++) {
-            for (int j = 0; j < resultado[i].length; j++) {
+            for (int j = 0; j < resultado[0].length; j++) {
                 System.out.print(resultado[i][j] + " ");
             }
             System.out.println();
         }
+    }
+
+
+    public static boolean esPar(int valor) {  //Función para determinar si un número es par
+        return valor % 2 == 0;
+    }
+
+    public static int contarAdyacentesPares(int[][] matriz, int fila, int col) {  //Función para contar números pares adyacentes
+        int contador = 0;
+        for (int i = fila - 1; i <= fila + 1; i++) {  //Dentro del rango de filas y columnas detecta las adyacentes incluidas las diagonales
+            for (int j = col - 1; j <= col + 1; j++) {
+                if (i == fila && j == col) continue;
+                if (i >= 0 && i < matriz.length && j >= 0 && j < matriz[0].length) {
+                    if (esPar(matriz[i][j])) contador++;
+                }
+            }
+        }
+        return contador;
+    }
+
+    public static int[][] generarMatrizResultado(int[][] matriz) {  //Función para generar la matriz resultado
+        int[][] resultado = new int[matriz.length][matriz[0].length];
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                resultado[i][j] = contarAdyacentesPares(matriz, i, j);
+            }
+        }
+        return resultado;
     }
 }
