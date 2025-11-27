dcdb063cab7edd915d76ed9a35b94a4add8da63e
yerayduran <yerayduranchaves2006@gmail.com>
2025-11-27 13:48:08 +0100

    27 de noviembre

---
+++ Tema 4/src/Boletin 4.2/src/Ejercicio6/Comercio.java
@@ -0,0 +1,11 @@
+package Ejercicio6;
+
+import java.util.Scanner;
+
+public class Comercio {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
---
+++ Tema 4/src/Boletin 4.2/src/Ejercicio6/Exception/ProductoException.java
@@ -0,0 +1,7 @@
+package Ejercicio6.Exception;
+
+public class ProductoException extends Exception {
+    public ProductoException(String message) {
+        super(message);
+    }
+}
